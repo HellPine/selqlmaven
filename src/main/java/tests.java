e7d931fa6e78ded62240f3515d627f74f0e360aa
HellPine <daniel.prado-pino@gtech.com>
2013-07-30 09:55:15 +0200

    more changes to report

--- src/main/java/tests.java
+++ src/main/java/tests.java
@@ -240,7 +240,7 @@ public class tests {
     	write2.write(result2);
     	write2.write("</tr></table>");
     	//String currentDir = System.getProperty("user.dir");
-    	write2.write("<p></p><p></p><p></p><p></p> Please follow this <a href="+(char)34+"./reports/"+ timesta + ".html"+(char)34+"> LINK </a> for a full report<p>");
+    	write2.write("<p></p><p></p><p></p><p></p> Please follow this <a href="+(char)34+ timesta + ".html"+(char)34+"> LINK </a> for a full report<p>");
     	
     	write.close();
 		write2.close();
